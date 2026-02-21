8a13c336cac3f635e2a3bce72de7df6f038ff6b3
BrutalChopp <nickotin.s@yandex.ru>
2026-02-21 22:30:15 +0300

    feat: SSL support via DuckDNS + Let's Encrypt

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -e
 
-# If DOMAIN is set, process the SSL template
 if [ -n "$DOMAIN" ] && [ -f /etc/nginx/nginx-ssl.template ]; then
-  echo "[entrypoint] SSL mode: substituting DOMAIN=$DOMAIN"
+  echo "[entrypoint] SSL mode: DOMAIN=$DOMAIN"
   envsubst '$DOMAIN' < /etc/nginx/nginx-ssl.template > /etc/nginx/conf.d/default.conf
+  echo "[entrypoint] SSL config activated for $DOMAIN"
 else
-  echo "[entrypoint] Standard HTTP mode"
+  echo "[entrypoint] Standard HTTP mode (no DOMAIN set)"
 fi
 
 exec nginx -g 'daemon off;'
